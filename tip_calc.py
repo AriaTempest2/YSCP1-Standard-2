209381d61a64e55dc3db3510db6709481e23bcb8
AriaTempest2 <148793715+AriaTempest2@users.noreply.github.com>
2025-02-26 14:07:31 +0000

    Done

--- tip_calc.py
+++ tip_calc.py
@@ -1,3 +1,15 @@
 # tip_calculator.py
 # A simple tip calculator to compute tip amount and total bill
+print("--- WELCOME TO TIP CALCULATOR ---\n")
+total = float(input("Enter the total bill amount: "))
+tip = float(input("Enter the tip percentage (e.g, 15 for 15 percent): "))
 
+full = tip / 100
+bill = total * full
+total_bill = total + bill
+
+print("\n--- TIP CALCULATOR SUMMARY ---")
+print(f"Bill Amount: {total}")
+print(f"Tip Amount: {tip}")
+print(f"Total Bill: {total_bill}")
+print("Thank you for using the Tip Calculator.")
